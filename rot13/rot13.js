a6e8b645616cde79b3fb77efaaac9eeb82a75b21
danielefrisanco <daniele.frisanco@gmail.com>
2014-11-27 17:36:27 +0100

    generalizzata la funzione rot e aggiunti i controlli per maiuscole e minuscole

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,17 +1,21 @@
 function rot13(s) {
 
-	var res = [];
-	for (var i = 0; i < s.length; i++) {
+	return rotX(s, 13);
 
-		if (s.charCodeAt(i) < 110) {
-			res[i] = String.fromCharCode(s.charCodeAt(i) + 13);
-		} else {
-			res[i] = String.fromCharCode(s.charCodeAt(i) - 13);
+}
+function rotX(s, x) {
+
+	var res = [];
+	var c;
+	for (var i = 0; i < s.length; i++) {
+		c = s.charCodeAt(i);
+		if ((c < 97 + x && c >= 97 && c <= 122) || (c < 65 + x && c >= 65 && c <= 90)) {
+			res[i] = String.fromCharCode(c + x);
+		} else if ((c >= 97 + x && c >= 97 && c <= 122) || (c >= 65 + x && c >= 65 && c <= 90)) {
+			res[i] = String.fromCharCode(c - x);
 		}
 	}
 
 	return res;
-	//return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < 'n' ? 13 : -13));
-
 
 }
