6d6831d6395cd293146509beffb9c2a559ec70bf
daniele frisanco <daniele.frisanco@gmail.com>
2014-11-28 14:17:53 +0100

    fatto al volo rot13 per ruby
    funziona, magari sistemare un po il codice

--- rot13/rot13.rb
+++ rot13/rot13.rb
@@ -1,49 +1,16 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
+def rot13(s)
 
-class rot_crypt
-  attr_accessor :x
+s  
+for i in 0..s.length - 1 
+  if  ( s[i]>=65 and  s[i]<=90 and s[i] <78)or(s[i]>=97 and  s[i]<=122 and s[i] <110 ) 
+      puts ( s[i]+13).chr
+elsif ( s[i]>=65 and  s[i]<=90 and s[i] >=78)or(s[i]>=97 and  s[i]<=122 and s[i]>=110 ) 
+      puts ( s[i]-13).chr
+ end
 
-  # Crea l'oggetto
-  def initialize(x=13)
-    @x = x
-  end
-
-  # cripta e decripta
-  def crypt
-    if @x.nil?
-      puts "error"
-    elsif @x.is_number?
-      # @x è un numero 
-      @nomi.each do |nome|
-        puts "Ciao #{nome}!"
-      end
-    else
-      puts "Ciao #{@nomi}!"
-    end
-  end
-function rot13(s) {
-
-	return rotX(s, 13);
-
-}
-function rotX(s, x) {
-
-	var res = [];
-	var c;
-	for (var i = 0; i < s.length; i++) {
-		c = s.charCodeAt(i);
-		if ((c < 97 + x && c >= 97 && c <= 122) || (c < 65 + x && c >= 65 && c <= 90)) {
-			res[i] = String.fromCharCode(c + x);
-		} else if ((c >= 97 + x && c >= 97 && c <= 122) || (c >= 65 + x && c >= 65 && c <= 90)) {
-			res[i] = String.fromCharCode(c - x);
-		}
-	}
-
-	return res;
-
-}
-
-
-if __FILE__ == $0
 end
+ 
+end
+rot13("extreme")
