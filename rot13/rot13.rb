6fa82d57eaede0899f0b2e621e97021ce85f8277
danielefrisanco <daniele.frisanco@gmail.com>
2014-11-29 17:45:47 +0100

    sistemato un po di errori che linterprete online non mi dava

--- rot13/rot13.rb
+++ rot13/rot13.rb
@@ -3,17 +3,17 @@
  
 #ruby rot13.rb extreme
 if __FILE__ == $0
-  
- ARGV.each do |a| a.split("").each do |x| 
-     if x.between?("A", "M") or x.between?("a","m")   
-	 then  print((x[0].ord+13 ).chr)   
-          
-    elsif x.between?("N","Z") or x.between?("n","z")  
-	then  print((x[0].ord-13 ).chr)
-    
-  end
-end
 
-  puts 
- end
- end
+	ARGV.each do |a|
+		a.split("").each do |x| 
+			if x.between?("A", "M") or x.between?("a","m")   
+				then  print((x[0].ord+13 ).chr)   
+				elsif x.between?("N","Z") or x.between?("n","z")  
+					then  print((x[0].ord-13 ).chr)
+
+			end
+		end
+
+		puts 
+	end
+end
