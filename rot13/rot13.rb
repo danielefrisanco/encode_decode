895c6acc426e68c27f9c68a73f763943cdf8b75b
danielefrisanco <daniele.frisanco@gmail.com>
2014-11-29 17:44:51 +0100

    sistemato un po di errori che linterprete online non mi dava

--- rot13/rot13.rb
+++ rot13/rot13.rb
@@ -1,32 +1,19 @@
 #!/usr/bin/ruby
 
-def rot13(s)
-
-s  
-for i in 0..s.length - 1 
-  if  ( s[i]>=65 and  s[i]<=90 and s[i] <78)or(s[i]>=97 and  s[i]<=122 and s[i] <110 ) 
-      puts ( s[i]+13).chr
-elsif ( s[i]>=65 and  s[i]<=90 and s[i] >=78)or(s[i]>=97 and  s[i]<=122 and s[i]>=110 ) 
-      puts ( s[i]-13).chr
- end
-
-end
  
-end
-rot13("extreme")
-
-
- 
- 
+#ruby rot13.rb extreme
+if __FILE__ == $0
   
-  
- 
- "extreme".split("").each do |x| 
-     if x.between?("A", "M") or x.between?("a","m")    then  print((x[0] +13).chr)   
+ ARGV.each do |a| a.split("").each do |x| 
+     if x.between?("A", "M") or x.between?("a","m")   
+	 then  print((x[0].ord+13 ).chr)   
           
-    elsif x.between?("N","Z") or x.between?("n","z")   then  print((x[0] -13).chr)
+    elsif x.between?("N","Z") or x.between?("n","z")  
+	then  print((x[0].ord-13 ).chr)
     
   end
 end
 
- 
+  puts 
+ end
+ end
